6959c2fb2c5e52f8977eb0dd277fd80d8aade529
Hunchojoe19 <enubiakjoseph@gmail.com>
2026-04-22 22:38:07 +0100

    style: apply custom-scrollbar class to overflow containers in UserAnalyticsTable, Layout, and Sidebar components

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -22,7 +22,7 @@ export default function Layout({ children, admin, onLogout }: LayoutProps) {
 			/>
 			<div className="flex-1 flex flex-col min-h-screen overflow-hidden bg-black">
 				<Header admin={admin} />
-				<main className="flex-1 p-6 lg:p-8 overflow-y-auto bg-background rounded-tl-[40px]">
+				<main className="flex-1 p-6 lg:p-8 overflow-y-auto bg-background rounded-tl-[40px] custom-scrollbar">
 					{children}
 				</main>
 			</div>
