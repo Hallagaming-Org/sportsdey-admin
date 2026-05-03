0e91aa2615e538456970d51ab3a94cd3f2082fe4
Hunchojoe19 <enubiakjoseph@gmail.com>
2026-05-03 12:43:29 +0100

    feat: replace sort and filter buttons with send notice action and relocate controls to search bar area

--- src/routes/app/users.tsx
+++ src/routes/app/users.tsx
@@ -175,17 +175,11 @@ function UsersPage() {
 					<p className="text-gray-600">Manage all your users and activities</p>
 				</div>
 				<div className="flex flex-wrap items-center gap-3">
-					<button
-						onClick={() => setSort((s) => (s === "asc" ? "desc" : "asc"))}
-						className="inline-flex cursor-pointer items-center gap-2 rounded-full border-2 border-primary px-2 py-2 font-medium text-gray-900 text-sm hover:bg-gray-50"
-					>
-						<SortIcon className="h-3 w-3" />
-						Sort
+					<button className="cursor-pointer flex items-center justify-center rounded-full text-[#053209] bg-[#F1F1F1] gap-x-3 w-[159px] h-11">
+						<NotificationIcon height={"15"} width={"15"} color={"#053209"} />
+						<span>Send a Notice</span>
 					</button>
-					<button className="inline-flex cursor-pointer items-center gap-2 rounded-full border-2 border-primary px-2 py-2 font-medium text-gray-900 text-sm hover:bg-gray-50">
-						<FilterIcon className="h-3 w-3" />
-						Filter
-					</button>
+					
 					<button
 						type="button"
 						onClick={() => setShowAddModal(true)}
@@ -255,6 +249,17 @@ function UsersPage() {
 							/>
 							<Search className="absolute top-1/2 left-3 h-4 w-4 -translate-y-1/2 text-gray-500" />
 						</div>
+						<button
+						onClick={() => setSort((s) => (s === "asc" ? "desc" : "asc"))}
+						className="inline-flex cursor-pointer items-center bg-[#F4F8F3] gap-2 rounded-full border-2 border-[#053209] px-2 py-2 font-medium text-gray-900 text-sm hover:bg-gray-50"
+					>
+						<SortIcon className="h-3 w-3" />
+						Sort
+					</button>
+					<button className="inline-flex cursor-pointer items-center bg-[#F4F8F3] gap-2 rounded-full border-2 border-[#053209] px-2 py-2 font-medium text-gray-900 text-sm hover:bg-gray-50">
+						<FilterIcon className="h-3 w-3" />
+						Filter
+					</button>
 						<button className="inline-flex items-center gap-1.5 rounded-lg bg-white px-3 py-2 text-xs font-medium text-gray-600 hover:bg-gray-50 cursor-pointer whitespace-nowrap">
 							<span className="hidden lg:block">Time periods</span>
 							<IoFilter className="h-3.5 w-3.5" />
